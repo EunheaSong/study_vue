2c18d3b4305b0f71503cc97e16e708771a3c779f
송은혜 <dmsgp8292@gmail.com>
2024-05-28 14:23:07 +0900

    attribute binging & conditional rendering

--- vue3/basic/main.js
+++ vue3/basic/main.js
@@ -2,6 +2,9 @@ const app = Vue.createApp({
   data: function () {
     return {
       product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      inventory: 0,
+      onSale: true,
     };
   },
 });
