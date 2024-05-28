96cc3c8e7db492788600e0bc0ba5e084bae15856
송은혜 <dmsgp8292@gmail.com>
2024-05-28 17:05:49 +0900

    computed properties

--- vue3/basic/main.js
+++ vue3/basic/main.js
@@ -3,14 +3,23 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Socks",
-      image: "./assets/images/socks_green.jpg",
-      inventory: 0,
-      onSale: true,
-      inStock: true,
+      brand: "Vue Mastrey",
+      selectedVariant: 0,
+      //   inStock: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quntity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quntity: 0,
+        },
       ],
     };
   },
@@ -18,8 +27,19 @@ const app = Vue.createApp({
     addToCart() {
       this.cart += 1;
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quntity;
     },
   },
 });
